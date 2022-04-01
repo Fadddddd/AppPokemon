83f0e9a8d563b9244e941eebbe8e67971627f5ff
fadoua <fadelhannachi@gmail.com>
2022-04-01 15:58:53 +0200

    settings

--- appokemon/src/App.js
+++ appokemon/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
@@ -12,15 +12,63 @@ const App = () => {
     try {
       const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
       const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemon(toArray);
       console.log(res);
     } catch (e) {
       console.log(e);
     }
   };
 
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase()); //parce que si on rentre les données avec majuscules, ça ne fonctionnera pas
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
   return (
     <div className="App">
-      <h1>Moi c'est Fad donc rien à voir ac Pokemon</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter Pokemon"
+          />
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            <img src={data.sprites["front_default"]}/>
+            <div className="divTable">
+              <div className="divTableBody"></div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Hauteur</div>
+                <div className="divTableCell">
+                  {""}
+                  {Math.round(data.height * 3.9 * 2.54)}cm
+                </div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Poids</div>
+                <div className="divTableCell">
+                  {""}
+                  {Math.round((data.weight / 4.3) * 0.453)}kg
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        );
+      })}
     </div>
   );
 };
